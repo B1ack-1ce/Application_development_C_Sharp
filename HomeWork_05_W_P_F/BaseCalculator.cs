fd24ee4921f1f4ed34fc3e9ee9f253d310c16467
Artemiy <aptem64@yandex.ru>
2024-02-05 18:45:15 +0300

    complete hm_05

---
+++ HomeWork_05_W_P_F/BaseCalculator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace HomeWork_05_W_P_F
+{
+    public class CalculatorArgs : EventArgs
+    {
+        public double Answer { get; set; }
+        public CalculatorArgs()
+        {
+            
+        }
+    }
+    internal class BaseCalculator
+    {
+        public double Result { get; private set; } = 0;
+        public EventHandler<CalculatorArgs> EventResult;
+
+        private Stack<double> stackOfResults = new Stack<double>();
+        public BaseCalculator() { }
+        private void Calculation()
+        {
+            EventResult?.Invoke(this, new CalculatorArgs { Answer = Result });
+        }
+        public bool Add(int value)
+        {
+            stackOfResults.Push(Result);
+            Result += value;
+            Calculation();
+            return true;
+        }
+        public bool Sub(int value)
+        {
+            stackOfResults.Push(Result);
+            Result -= value;
+            Calculation();
+            return true;
+        }
+        public bool Multi(int value)
+        {
+            stackOfResults.Push(Result);
+            Result *= value;
+            Calculation();
+            return true;
+        }
+        public bool Div(int value)
+        {
+            stackOfResults.Push(Result);
+            Result /= value;
+            Calculation();
+            return true;
+        }
+        public bool Cancel()
+        {
+            if(stackOfResults.Count > 0)
+            {
+                Result = stackOfResults.Pop();
+                Calculation();
+                return true;
+            }
+            return false;
+        }
+        public bool Reset()
+        {
+            Result = 0;
+            stackOfResults = new Stack<double>();
+            Calculation();
+            return true;
+        }
+    }
+}
---
+++ HomeWork_05_W_P_F/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HomeWork_05_W_P_F
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculator calculator;
+        public MainWindow()
+        {
+            InitializeComponent();
+            calculator = new Calculator();
+            calculator.EventResult += CalculatorResult;
+        }
+        private void CalculatorResult(object? sender, CalculatorArgs e)
+        {
+            Answer.Content = e.Answer;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string input = InputText.Text;
+            StringBuilder res = new StringBuilder();
+
+            foreach (char c in input)
+            {
+                if (c >= '0' && c <= '9')
+                    res.Append(c);
+            }
+            int value = 0;
+            if(res.Length > 0)
+                int.TryParse(res.ToString(), out value);
+
+            string? name = (e.Source as FrameworkElement)?.Name;
+            switch (name)
+            {
+                case "Add":
+                    calculator.Add(value);
+                    InputText.Text = string.Empty;
+                    break;
+                case "Sub":
+                    calculator.Sub(value);
+                    InputText.Text = string.Empty;
+                    break;
+                case "Multi":
+                    calculator.Multi(value);
+                    InputText.Text = string.Empty;
+                    break;
+                case "Div":
+                    calculator.Div(value);
+                    InputText.Text = string.Empty;
+                    break;
+                case "Cancel":
+                    calculator.Cancel();
+                    InputText.Text = string.Empty;
+                    break;
+                case "Reset":
+                    calculator.Reset();
+                    InputText.Text = string.Empty;
+                    break;
+                default:
+                    MessageBox.Show($"Неизвестная операция: {name}", "Ошибка ввода");
+                    break;
+            }
+        }
+
+        
+    }
+}
